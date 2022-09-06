f86af004ab8b9e2c26e989f14f631f1ea83cf8a6
Ezequiel Hermoso <hermosoezequiel352@gmail.com>
2022-09-06 18:16:00 -0300

    segundo desafio simulador

--- app.js
+++ app.js
@@ -1,70 +1,66 @@
 
 
-
-//PRIMER DESAFIO
-/*
-let i = 1;
-let numero = Number(prompt("ingrese un numero menor a 10"))
-
-while(i< 10){
-    if(numero >= 10){
-       break;
-    }
-    console.log(i)
-    i++;
-    let resultado = numero + i;
-    alert(`${numero} + ${i} = ${resultado}`)
-    
-}
-alert("fin de la suma o tu numero es mayor o igual a 10")
-  */
-    
-    
-
-
-
-
 //SEGUNDO DESAFIO
 
 
-let pagar = 6000;
 let usuario = prompt("ingrese su usuario");
 
 
-alert("bienvenido/a " + usuario + " su monto a pagar es de " + "$" + pagar)
-elejir();
+alert("bienvenido/a " + usuario )
 
 
-function elejir()  {
+let productos = prompt(`Coloque el numero del producto que usted desee comprar:
+1. cable Soul V8 $700
+2. Monitor Samsung $10000
+3. Teclado Noga $4000`);
 
-for ( opciones = 0 ; opciones == 1,3,6,9,12; opciones++ ) {
-    let opciones = Number(prompt("Escriba con la cantidad de cuotas con la cula usted desea pagar: \n 1 cuota \n 3 coutas \n 6 cuotas \n 9 cuotas \n 12 coutas"))
-
-
-if (opciones == 1) {
-    let resultado = pagar;
-    alert ("efectuara el pago de 1 cuota de " + resultado)
-    break;
-} else if (opciones == 3){
-    let resultado2 = pagar / 3;
-    alert ("efectuara el pago de 3 cuota de " + resultado2)
-    break;
-} else if(opciones == 6){
-    let resultado3 = pagar / 6;
-    alert ("efectuara el pago de 6 cuota de: " + resultado3)
-    break;
-} else if(opciones == 9){
-    let resultado4 = pagar / 9 ;
-    alert ("efectuara el pago de 9 cuota de " + resultado4)
-    break;
-} else if(opciones == 12){
-    let resultado5 = pagar / 12;
-    alert ("efectuara el pago de 12 cuota de " + resultado5)
-    break;
+const preciosProductos  = {
+    "1" : 700,
+    "2" : 10000,
+    "3" : 4000
 }
-else {
-    alert("El numero de cuotas ingresado es incorrecto")
+const nombreProductos = {
+    "1" : "cable soul v8",
+    "2" : "Monitor Samsung",
+    "3" : "Teclado Noga"
 }
-}
+
+alert("producto seleccionado: " +  nombreProductos[productos] + " con un valor de $" + preciosProductos[productos] )
+alert ("Producto agregado correctamente, pasaremos a la forma de pago")
+
+let opciones = Number(prompt(`Escriba con la cantidad de cuotas con la cual usted desea pagar: 
+1 cuota 
+3 coutas 
+6 cuotas 
+9 cuotas 
+12 coutas`))
+
+calcularCuotas()
+function calcularCuotas(){
+    switch (opciones){
+        case 1:
+            let resultado = preciosProductos[productos] / 1;
+            alert ("efectuara el pago de 1 cuota de " + resultado);
+            break;
+        case 3:
+            let resultado2 = preciosProductos[productos] / 3;
+            alert ("efectuara el pago de 3 cuota de " + resultado2);
+            break;
+        case 6:
+            let resultado3 = preciosProductos[productos] / 6;
+            alert ("efectuara el pago de 6 cuota de: " + resultado3)
+            break;
+        case 9:
+            let resultado4 =  preciosProductos[productos] / 9;
+            alert ("efectuara el pago de 9 cuota de " + resultado4)
+            break;
+        case 12:
+            let resultado5 =  preciosProductos[productos] / 12;
+            alert ("efectuara el pago de 12 cuota de " + resultado5)
+            break;
+        default :
+            break;
+        }
 }
 alert("el pago se realizo con exito")
+alert("Gracias por su compra")
