d8ebd1b7695f04d6f81de8af98d75cebea323db9
Ezequiel Hermoso <hermosoezequiel352@gmail.com>
2022-09-15 21:32:25 -0300

    correccion del desafio

--- app.js
+++ app.js
@@ -3,21 +3,15 @@
 //SEGUNDO DESAFIO
 
 
-let usuario = prompt("ingrese su usuario");
+let usuario = prompt("ingrese su Nombre");
 
 
 alert("bienvenido/a " + usuario )
 
-
-let productos = prompt(`Coloque el numero del producto que usted desee comprar:
-1. cable Soul V8 $700
-2. Monitor Samsung $10000
-3. Teclado Noga $4000`);
-
 const preciosProductos  = {
     "1" : 700,
-    "2" : 10000,
-    "3" : 4000
+    "2" : 9000,
+    "3" : 3000
 }
 const nombreProductos = {
     "1" : "cable soul v8",
@@ -25,42 +19,95 @@ const nombreProductos = {
     "3" : "Teclado Noga"
 }
 
-alert("producto seleccionado: " +  nombreProductos[productos] + " con un valor de $" + preciosProductos[productos] )
-alert ("Producto agregado correctamente, pasaremos a la forma de pago")
 
-let opciones = Number(prompt(`Escriba con la cantidad de cuotas con la cual usted desea pagar: 
-1 cuota 
-3 coutas 
-6 cuotas 
-9 cuotas 
-12 coutas`))
+let productos = prompt(`Coloque el numero del producto que usted desee comprar:
+1. cable Soul V8 $700
+2. Monitor Samsung $10000
+3. Teclado Noga $4000
+4. (Para salir)`);
 
-calcularCuotas()
-function calcularCuotas(){
+EleccionUsuarioInc ()
+
+function  EleccionUsuarioInc(){
+
+    while (productos != "1" && productos != "2" && productos !="3" && productos != "4"){
+        alert("Por favor ingrese un numero correcto")
+        productos = prompt(`Coloque el numero del producto que usted desee comprar:
+        1. cable Soul V8 $700
+        2. Monitor Samsung $10000
+        3. Teclado Noga $4000
+        4. (Para salir)`);
+    }
+
+}
+
+comparacionEleccionUsua ()
+
+function comparacionEleccionUsua (){
+    if (productos == "1" || productos == "2" || productos == "3") {
+        alert("producto seleccionado: " +  nombreProductos[productos] + " con un valor de $" + preciosProductos[productos] )
+        alert ("Producto agregado correctamente, pasaremos a la forma de pago")
+
+    } else if (productos == "4"){
+        alert("Gracias por ingresar a nuestra pagina")
+    }
+}
+
+
+
+let opciones;
+
+    while (productos != "4") {
+    
+        opciones = Number(prompt(`Escriba con la cantidad de cuotas con la cual usted desea pagar: 
+            1 cuota 
+            3 coutas 
+            6 cuotas 
+            9 cuotas 
+            12 coutas`))
+
+
+        if (opciones != 1 && opciones != 3 && opciones != 6 && opciones != 9 && opciones != 12 ) {
+        
+            alert("no ingreso cuotas correctas")
+            opciones = Number(prompt(`Escriba con la cantidad de cuotas con la cual usted desea pagar: 
+            1 cuota 
+            3 coutas 
+            6 cuotas 
+            9 cuotas 
+            12 coutas`))
+            
+    
+
+        } else if (opciones == 1 || opciones == 3 || opciones == 6 || opciones == 9 || opciones == 12){
+            calcularCuotas(productos, opciones)
+            alert("el pago se realizo con exito")
+            alert("Gracias por su compra")
+            break
+        }
+    }
+    
+
+function calcularCuotas(productos, cuotas){
+    let resultado = preciosProductos[productos] / cuotas;
+    
     switch (opciones){
         case 1:
-            let resultado = preciosProductos[productos] / 1;
             alert ("efectuara el pago de 1 cuota de " + resultado);
             break;
         case 3:
-            let resultado2 = preciosProductos[productos] / 3;
-            alert ("efectuara el pago de 3 cuota de " + resultado2);
+            alert ("efectuara el pago de 3 cuota de " + resultado);
             break;
         case 6:
-            let resultado3 = preciosProductos[productos] / 6;
-            alert ("efectuara el pago de 6 cuota de: " + resultado3)
+            alert ("efectuara el pago de 6 cuota de: " + resultado)
             break;
         case 9:
-            let resultado4 =  preciosProductos[productos] / 9;
-            alert ("efectuara el pago de 9 cuota de " + resultado4)
+            alert ("efectuara el pago de 9 cuota de " + resultado)
             break;
         case 12:
-            let resultado5 =  preciosProductos[productos] / 12;
-            alert ("efectuara el pago de 12 cuota de " + resultado5)
+            alert ("efectuara el pago de 12 cuota de " + resultado)
             break;
         default :
             break;
         }
 }
-alert("el pago se realizo con exito")
-alert("Gracias por su compra")
